9e4d55e067e62ad9cd8a78c31b9519cf637cf686
karoatani <karoatani@gmail.com>
2025-01-25 21:01:26 +0100

    Fix some issues with prod

--- parent_guard/settings.py
+++ parent_guard/settings.py
@@ -84,18 +84,19 @@ WSGI_APPLICATION = 'parent_guard.wsgi.application'
 
 # Production Database
 
-if DEBUG:
-    DB_NAME = config("DB_NAME")
-    DB_USER = config("DB_USER")
-    DB_PASSWORD = config("DB_PASSWORD")
-    DATABASES = {
+DB_NAME = config("DB_NAME")
+DB_USER = config("DB_USER")
+DB_PASSWORD = config("DB_PASSWORD")
+DB_HOST = config("DB_HOST")
+DB_PORT = config("DB_PORT")
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': DB_NAME,
             'USER': DB_USER,
             'PASSWORD': DB_PASSWORD,
-            'HOST': 'localhost',
-            'PORT': '5432',
+            'HOST': DB_HOST,
+            'PORT': DB_PORT,
         }
     }
 
