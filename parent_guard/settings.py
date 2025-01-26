2657f10ecde62f2a207cd2f63fdef0af708448c5
karoatani <karoatani@gmail.com>
2025-01-26 07:10:15 +0100

    Fixing images not working in prod

--- parent_guard/settings.py
+++ parent_guard/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2%zniofp135at)44p4^w=er=xq(-fe%bsy_)3=x8nuc^ivyjco'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
