4bfaa8d86fe676f1cdfc74bfe03397285bbaa2d9
karoatani <karoatani@gmail.com>
2025-01-26 09:48:32 +0100

    minor fixes

--- core/views.py
+++ core/views.py
@@ -159,9 +159,9 @@ class BrowsingSessionStartAPIView(APIView):
         blocked_by_schedule = child.schedule.filter(
             duration_start__lte=current_time,
             duration_end__gte=current_time
-        ).exists()
+        )
+        print('activeeeee')
         print(blocked_by_schedule)
-        print(child.schedule.all())
         if blocked_by_schedule:
             # Log blocked access attempt
             ActivityLog.objects.create(
